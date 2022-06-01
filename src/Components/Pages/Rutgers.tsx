37d48973312061326455409914a817d7cf0427f8
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2022-06-01 13:41:42 -0400

    Styled the info section of the card

--- src/Components/Pages/Rutgers.tsx
+++ src/Components/Pages/Rutgers.tsx
@@ -13,32 +13,37 @@ export const Rutgers = () => {
             {
                 Courses.map((course, idx) => {
                     return (
-                        <article key={`Course-${idx}`} className="class">
-                            <header aria-label="class header" className="class-header">
-                                <h2 className="h3">
-                                    <a href={course.link} rel="noreferrer noopener" target="_blank">
-                                        {course.title}
-                                    </a>
-                                </h2>
-                                <h2 className="class-professor h4">
-                                    Instructor {course.professor} 
-                                </h2>
-                            </header>
-                            <div aria-label="class description" className="class-description text">
-                                {course.description}
-                            </div>
-                            <div aria-label="class footer" className="class-footer">
-                                {
-                                    course.resources.map((resource, _idx) => {
-                                        return (
-                                            <div key={`class-footer${idx}-${_idx}`}>
-                                                {resource}
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        </article>
+                        <CourseCard 
+                            course={course} 
+                            key={`Course-${idx}`}
+                        />
+
+                        // <article key={`Course-${idx}`} className="class">
+                        //     <header aria-label="class header" className="class-header">
+                        //         <h2 className="h3">
+                        //             <a href={course.link} rel="noreferrer noopener" target="_blank">
+                        //                 {course.title}
+                        //             </a>
+                        //         </h2>
+                        //         <h2 className="class-professor h4">
+                        //             Instructor {course.professor} 
+                        //         </h2>
+                        //     </header>
+                        //     <div aria-label="class description" className="class-description text">
+                        //         {course.description}
+                        //     </div>
+                        //     <div aria-label="class footer" className="class-footer">
+                        //         {
+                        //             course.resources.map((resource, _idx) => {
+                        //                 return (
+                        //                     <div key={`class-footer${idx}-${_idx}`}>
+                        //                         {resource}
+                        //                     </div>
+                        //                 )
+                        //             })
+                        //         }
+                        //     </div>
+                        // </article>
                     )
                 })
             }
